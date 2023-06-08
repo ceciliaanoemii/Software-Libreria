17698053e17eac94f5a6dd37bac22e7ca4ce34e9
Lucentinela <luciapereyra.pl@gmail.com>
2023-06-07 22:35:12 -0300

    Init frontend

---
+++ Software-Libreria/Dashboard.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_Libreria
+{
+    public partial class WindowDashboard : Form
+    {
+        string nombre_vendedor;
+        string contrasenia_vendedor;
+        public WindowDashboard(string usuario, string contrasenia)
+        {
+            InitializeComponent();
+            nombre_vendedor = usuario;
+            contrasenia_vendedor = contrasenia;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowDashboard_Load(object sender, EventArgs e)
+        {
+            label1.Text += nombre_vendedor;
+        }
+    }
+}
---
+++ Software-Libreria/Login.cs
@@ -0,0 +1,56 @@
+using System.Windows.Forms;
+
+namespace Software_Libreria
+{
+    public partial class WindowLogin : Form
+    {
+        public WindowLogin()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            btnIngresar.Enabled = false;
+        }
+
+        private void controlBotones()
+        {
+            if (nombreUsuario.Text.Trim() != string.Empty && nombreUsuario.Text.All(Char.IsLetter))
+            {
+                btnIngresar.Enabled = true;
+                errorProvider1.SetError(nombreUsuario, "");
+            }
+            else
+            {
+                if (!nombreUsuario.Text.All(Char.IsLetter))
+                {
+                    errorProvider1.SetError(nombreUsuario, "El nombre sólo debe contener letras");
+                }
+                else
+                {
+                    errorProvider1.SetError(nombreUsuario, "Debe introducir su nombre");
+                }
+                btnIngresar.Enabled = false;
+                nombreUsuario.Focus();
+            }
+
+        }
+        private void nombreUsuario_TextChanged(object sender, EventArgs e)
+        {
+            controlBotones();
+        }
+
+        private void Contrasenia_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Ingresar_Click(object sender, EventArgs e)
+        {
+            using (WindowDashboard winDashboard = new WindowDashboard(nombreUsuario.Text, contrasenia.Text))
+                winDashboard.ShowDialog();
+        }
+    }
+}
