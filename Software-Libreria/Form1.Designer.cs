18596d2ded9279a6c50abd83dfb0062d7ebcb3b7
ceciliaanoemii <cecilianmogro@gmail.com>
2023-06-08 19:33:16 -0300

    cambios

--- Software-Libreria/Form1.Designer.cs
+++ Software-Libreria/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            Prueba = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(68, 55);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(599, 270);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Lucia";
+            // 
+            // Prueba
+            // 
+            Prueba.Location = new Point(555, 347);
+            Prueba.Name = "Prueba";
+            Prueba.Size = new Size(112, 34);
+            Prueba.TabIndex = 1;
+            Prueba.Text = "Prueba";
+            Prueba.UseVisualStyleBackColor = true;
+            Prueba.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(884, 457);
+            Controls.Add(Prueba);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button Prueba;
     }
 }
