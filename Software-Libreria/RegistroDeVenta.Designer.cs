02939392303f1d0382abb4063515809b375ff0f2
Andres Angel <developerworld33@gmail.com>
2023-06-24 01:54:05 -0300

    Pdf

--- Software-Libreria/RegistroDeVenta.Designer.cs
+++ Software-Libreria/RegistroDeVenta.Designer.cs
@@ -51,6 +51,7 @@
             columnHeader5 = new ColumnHeader();
             columnHeader6 = new ColumnHeader();
             columnHeader7 = new ColumnHeader();
+            btnGenFac = new Button();
             groupBox1.SuspendLayout();
             SuspendLayout();
             // 
@@ -58,71 +59,72 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(282, 9);
+            label1.Location = new Point(322, 12);
             label1.Name = "label1";
-            label1.Size = new Size(225, 32);
+            label1.Size = new Size(278, 41);
             label1.TabIndex = 0;
             label1.Text = "Registro de ventas";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(553, 38);
+            label2.Location = new Point(632, 51);
             label2.Name = "label2";
-            label2.Size = new Size(79, 15);
+            label2.Size = new Size(98, 20);
             label2.TabIndex = 1;
             label2.Text = "Fecha actual :";
             // 
             // lblFecha
             // 
             lblFecha.AutoSize = true;
-            lblFecha.Location = new Point(638, 38);
+            lblFecha.Location = new Point(729, 51);
             lblFecha.Name = "lblFecha";
-            lblFecha.Size = new Size(51, 15);
+            lblFecha.Size = new Size(64, 20);
             lblFecha.TabIndex = 2;
             lblFecha.Text = "lblFecha";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(6, 19);
+            label4.Location = new Point(7, 25);
             label4.Name = "label4";
-            label4.Size = new Size(93, 15);
+            label4.Size = new Size(118, 20);
             label4.TabIndex = 3;
             label4.Text = "Seleccione libro:";
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(7, 72);
+            label5.Location = new Point(8, 96);
             label5.Name = "label5";
-            label5.Size = new Size(136, 15);
+            label5.Size = new Size(174, 20);
             label5.TabIndex = 4;
             label5.Text = "Seleccione tipo de pago:";
             // 
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(233, 19);
+            label6.Location = new Point(266, 25);
             label6.Name = "label6";
-            label6.Size = new Size(58, 15);
+            label6.Size = new Size(72, 20);
             label6.TabIndex = 5;
             label6.Text = "Cantidad:";
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(571, 19);
+            label7.Location = new Point(653, 25);
             label7.Name = "label7";
-            label7.Size = new Size(86, 15);
+            label7.Size = new Size(109, 20);
             label7.TabIndex = 6;
             label7.Text = "Precio de libro:";
             // 
             // btnCancelar
             // 
-            btnCancelar.Location = new Point(313, 239);
+            btnCancelar.Location = new Point(358, 319);
+            btnCancelar.Margin = new Padding(3, 4, 3, 4);
             btnCancelar.Name = "btnCancelar";
-            btnCancelar.Size = new Size(75, 23);
+            btnCancelar.Size = new Size(86, 31);
             btnCancelar.TabIndex = 7;
             btnCancelar.Text = "Cancelar";
             btnCancelar.UseVisualStyleBackColor = true;
@@ -130,9 +132,10 @@
             // 
             // btnRegistrar
             // 
-            btnRegistrar.Location = new Point(394, 239);
+            btnRegistrar.Location = new Point(450, 319);
+            btnRegistrar.Margin = new Padding(3, 4, 3, 4);
             btnRegistrar.Name = "btnRegistrar";
-            btnRegistrar.Size = new Size(75, 23);
+            btnRegistrar.Size = new Size(86, 31);
             btnRegistrar.TabIndex = 8;
             btnRegistrar.Text = "Registrar";
             btnRegistrar.UseVisualStyleBackColor = true;
@@ -140,9 +143,10 @@
             // 
             // btnSalir
             // 
-            btnSalir.Location = new Point(679, 415);
+            btnSalir.Location = new Point(776, 553);
+            btnSalir.Margin = new Padding(3, 4, 3, 4);
             btnSalir.Name = "btnSalir";
-            btnSalir.Size = new Size(75, 23);
+            btnSalir.Size = new Size(86, 31);
             btnSalir.TabIndex = 9;
             btnSalir.Text = "Salir";
             btnSalir.UseVisualStyleBackColor = true;
@@ -152,9 +156,9 @@
             // 
             lblPrecio.AutoSize = true;
             lblPrecio.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
-            lblPrecio.Location = new Point(564, 38);
+            lblPrecio.Location = new Point(645, 51);
             lblPrecio.Name = "lblPrecio";
-            lblPrecio.Size = new Size(104, 30);
+            lblPrecio.Size = new Size(137, 37);
             lblPrecio.TabIndex = 10;
             lblPrecio.Text = "LblPrecio";
             // 
@@ -168,9 +172,11 @@
             groupBox1.Controls.Add(label4);
             groupBox1.Controls.Add(label5);
             groupBox1.Controls.Add(label7);
-            groupBox1.Location = new Point(21, 66);
+            groupBox1.Location = new Point(24, 88);
+            groupBox1.Margin = new Padding(3, 4, 3, 4);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(767, 122);
+            groupBox1.Padding = new Padding(3, 4, 3, 4);
+            groupBox1.Size = new Size(877, 163);
             groupBox1.TabIndex = 11;
             groupBox1.TabStop = false;
             groupBox1.Text = "Control de ventas";
@@ -179,33 +185,37 @@
             // 
             comboSeleccionTipoPago.FormattingEnabled = true;
             comboSeleccionTipoPago.Items.AddRange(new object[] { "Efectivo", "Mercado Pago", "Tarjeta" });
-            comboSeleccionTipoPago.Location = new Point(7, 93);
+            comboSeleccionTipoPago.Location = new Point(8, 124);
+            comboSeleccionTipoPago.Margin = new Padding(3, 4, 3, 4);
             comboSeleccionTipoPago.Name = "comboSeleccionTipoPago";
-            comboSeleccionTipoPago.Size = new Size(210, 23);
+            comboSeleccionTipoPago.Size = new Size(239, 28);
             comboSeleccionTipoPago.TabIndex = 15;
             // 
             // comboSeleccionLibro
             // 
             comboSeleccionLibro.FormattingEnabled = true;
             comboSeleccionLibro.Items.AddRange(new object[] { "Harry Potter - Las reliquias de la muerte", "El señor de los anillos - Las dos torres", "Agatha Christie - Asesinado en Mesopotamia" });
-            comboSeleccionLibro.Location = new Point(6, 38);
+            comboSeleccionLibro.Location = new Point(7, 51);
+            comboSeleccionLibro.Margin = new Padding(3, 4, 3, 4);
             comboSeleccionLibro.Name = "comboSeleccionLibro";
-            comboSeleccionLibro.Size = new Size(211, 23);
+            comboSeleccionLibro.Size = new Size(241, 28);
             comboSeleccionLibro.TabIndex = 13;
             // 
             // txtCantidad
             // 
-            txtCantidad.Location = new Point(233, 38);
+            txtCantidad.Location = new Point(266, 51);
+            txtCantidad.Margin = new Padding(3, 4, 3, 4);
             txtCantidad.Name = "txtCantidad";
-            txtCantidad.Size = new Size(100, 23);
+            txtCantidad.Size = new Size(114, 27);
             txtCantidad.TabIndex = 14;
             // 
             // listVenta
             // 
             listVenta.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4, columnHeader5, columnHeader6, columnHeader7 });
-            listVenta.Location = new Point(21, 280);
+            listVenta.Location = new Point(24, 373);
+            listVenta.Margin = new Padding(3, 4, 3, 4);
             listVenta.Name = "listVenta";
-            listVenta.Size = new Size(767, 129);
+            listVenta.Size = new Size(876, 171);
             listVenta.TabIndex = 12;
             listVenta.UseCompatibleStateImageBehavior = false;
             listVenta.View = View.Details;
@@ -238,11 +248,23 @@
             // 
             columnHeader7.Text = "Precio Final";
             // 
+            // btnGenFac
+            // 
+            btnGenFac.Location = new Point(617, 553);
+            btnGenFac.Margin = new Padding(3, 4, 3, 4);
+            btnGenFac.Name = "btnGenFac";
+            btnGenFac.Size = new Size(152, 31);
+            btnGenFac.TabIndex = 13;
+            btnGenFac.Text = "Generar Factura";
+            btnGenFac.UseVisualStyleBackColor = true;
+            btnGenFac.Click += btnGenFac_Click;
+            // 
             // RegistroDeVenta
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
+            Controls.Add(btnGenFac);
             Controls.Add(listVenta);
             Controls.Add(groupBox1);
             Controls.Add(btnSalir);
@@ -251,6 +273,7 @@
             Controls.Add(lblFecha);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "RegistroDeVenta";
             Text = "Registro de venta";
             Load += RegistroDeVenta_Load;
@@ -270,12 +293,12 @@
         private Label label6;
         private Label label7;
         private Button btnCancelar;
-        private Button btnRegistrar;
+        public Button btnRegistrar;
         private Button btnSalir;
         private Label lblPrecio;
         private GroupBox groupBox1;
         private TextBox txtCantidad;
-        private ListView listVenta;
+        public ListView listVenta;
         private ComboBox comboSeleccionTipoPago;
         private ComboBox comboSeleccionLibro;
         private ColumnHeader columnHeader1;
@@ -285,5 +308,6 @@
         private ColumnHeader columnHeader5;
         private ColumnHeader columnHeader6;
         private ColumnHeader columnHeader7;
+        private Button btnGenFac;
     }
 }
